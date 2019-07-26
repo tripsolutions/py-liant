32f2a76e3dedda9350cada80e43b45a4b38ae3fa
George Barbarosie <george.barbarosie@gmail.com>
2019-07-26 11:25:48 +0100

    add pool listener to set the correct search path to be used for projects that use only one schema and need to be hosted in shared database

---
+++ py_liant/searchpath.py
@@ -0,0 +1,34 @@
+from sqlalchemy.interfaces import PoolListener
+import logging
+
+sa_logger = logging.getLogger('sqlalchemy')
+
+
+class SearchPathSetter(PoolListener):
+    '''
+    Dynamically sets the search path on connections checked out from a pool.
+    '''
+
+    def __init__(self, search_path='public'):
+        self.search_path = search_path
+
+    @staticmethod
+    def quote_schema(dialect, schema):
+        return dialect.identifier_preparer.quote_schema(schema, False)
+
+    def checkout(self, dbapi_con, con_record, con_proxy):
+        dialect = con_proxy._pool._dialect
+        search_path = [
+            self.quote_schema(dialect, _.strip())
+            for _ in self.search_path.split(',')
+            if _.strip()
+        ]
+        if 'public' not in search_path:
+            search_path.append('public')
+        cursor = dbapi_con.cursor()
+        statement = "SET search_path TO %s;" % ', '.join(search_path)
+        print(statement)
+        sa_logger.info(statement)
+        cursor.execute(statement)
+        dbapi_con.commit()
+        cursor.close()
